a5b62dfb0d595f9887ae812c47f65f9e814fee27
rmansuri <mansurirushda7@gmail.com>
2023-10-13 04:07:11 -0400

    updated the Navbar

--- website/client/src/components/Navbar.jsx
+++ website/client/src/components/Navbar.jsx
@@ -37,36 +37,9 @@ const Navbar = () => {
           </li>
           {/* Services Dropdown */}
           <li className="relative group">
-            <Link to = "/in-progress" className="text-gray-800 hover:text-gray-500">
+            <Link to = "/Service" className="text-gray-800 hover:text-gray-500">
               Services
             </Link>
-            <ul className="absolute left-0 mt-1 space-y-1 text-gray-800 bg-white border border-gray-300 rounded-md shadow-md hidden group-hover:block w-64 transition-all duration-300 ease-in-out">
-              <li>
-                <Link to = "/in-progress" className="block w-full px-6 py-2 text-sm hover:bg-gray-100 whitespace-nowrap">
-                  Cleaning Services
-                </Link>
-              </li>
-              <li>
-                <Link to = "/in-progress" className="block w-full px-6 py-2 text-sm hover:bg-gray-100 whitespace-nowrap">
-                  Laundry Services
-                </Link>
-              </li>
-              <li>
-                <Link to = "/in-progress" className="block w-full px-6 py-2 text-sm hover:bg-gray-100 whitespace-nowrap">
-                  Lawn and Garden Services
-                </Link>
-              </li>
-              <li>
-                <Link to = "/in-progress" className="block w-full px-6 py-2 text-sm hover:bg-gray-100 whitespace-nowrap">
-                  Personal Chef Services
-                </Link>
-              </li>
-              <li>
-                <Link to = "/in-progress" className="block w-full px-6 py-2 text-sm hover:bg-gray-100 whitespace-nowrap">
-                  Handyman Services
-                </Link>
-              </li>
-            </ul>
           </li>
           <li>
             <Link to = "/in-progress" className="text-gray-800 hover:text-gray-500">
